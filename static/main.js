be242648151b5f113974b2199cdda11afe296499
Vladimir Abiduev <gunlinux@ya.ru>
2021-04-28 04:25:01 +0300

    prototype

---
+++ static/main.js
@@ -0,0 +1,65 @@
+var conf = {
+    'chmode': false,
+//  'chmult': false,
+    'insane': true
+}
+
+
+var chmodes = [
+    'No death',
+    'No Autoguns',
+    'Revolver / Pistols only',
+    'No Bullet time'
+];
+
+
+var insanes = [
+    '-No cutscene skipper-',
+    '-All tv run-',
+    '-Pacifist run-'
+];
+
+
+
+var gamemods = [
+    'easy',
+    'normal',
+    'hard',
+    'hardcore',
+    'oldschool',
+    'nyhm',
+    'score attack'
+];
+
+
+function getr(items) {
+    return items[Math.floor(Math.random() * items.length)];
+}
+
+function genmode() {
+    var gamemod = getr(gamemods);
+    console.log(gamemod);
+    return gamemod;
+}
+
+function loadconfig() {
+    conf.chmode = $('#chmode').is(':checked');
+    conf.insane = $('#insane').is(':checked');
+    console.log(conf);
+}
+
+$('#main').submit (function() {
+    loadconfig();
+    var gamemode = genmode();
+    var chmode = '';
+    var insanemode = '';
+    if (conf.chmode) {
+        chmode = getr(chmodes);
+    }
+    if (conf.insane) {
+        insanemode = getr(insanes);
+    }
+    $('#result').html(gamemode + ' ' + chmode + ' ' + insanemode);
+    $('#result').removeClass('hidden');
+    return false;
+});$
